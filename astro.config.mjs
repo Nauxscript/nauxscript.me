67cef39c09d6c9478e2f3a1d44fe6b900e3f75b4
nauxscript mac-mini <nauxscript@gmail.com>
2022-08-24 22:14:50 +0800

    :construction: setup unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
+import Unocss from '@unocss/astro'
+import presetIcons from '@unocss/preset-icons'
+import transformerDirectives from '@unocss/transformer-directives'
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    Unocss({
+      presets: [presetIcons({
+        prefix: 'i-',
+        extraProperties: {
+          display: 'inline-block'
+        }
+      })],
+      transformers: [
+        transformerDirectives()
+      ]
+    })
+  ]
+});

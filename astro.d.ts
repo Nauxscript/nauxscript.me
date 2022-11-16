995549551f1d079b74e3a0abc4ee480082832962
nauxscript <nauxscript@gmail.com>
2022-11-16 23:10:12 +0800

    chore: type fix

--- astro.d.ts
+++ astro.d.ts
@@ -2,6 +2,6 @@ import type { AttributifyAttributes } from '@unocss/preset-attributify'
 
 declare global {
   namespace astroHTML.JSX {
-    interface HTMLAttributes extends AttributifyAttributes {}
+    interface HTMLAttributes extends AttributifyAttributes { }
   }
 }

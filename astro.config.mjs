3abf68348b8975fe1a1b29a08fa491761f637096
nauxscript <nauxscript@gmail.com>
2022-11-30 09:21:09 +0800

    chore: migrate uno config to a separate file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,9 @@
 import { defineConfig } from 'astro/config';
-// import Unocss from '@unocss/astro'
-// fixed unocss hmr bug, see in issue: https://github.com/withastro/astro/issues/3291
-import Unocss from 'astro-uno'
-import { presetAttributify, presetUno } from 'unocss';
-import presetIcons from '@unocss/preset-icons'
-import transformerDirectives from '@unocss/transformer-directives'
-
+import Unocss from '@unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    base: './'
-  },
   integrations: [
-    Unocss({
-      presets: [
-        presetAttributify(),
-        presetUno({
-          dark: 'class',
-        }),
-        presetIcons({
-          prefix: 'i-',
-          extraProperties: {
-            display: 'inline-block'
-          }
-        })
-      ],
-      transformers: [
-        transformerDirectives()
-      ],
-      rules: [
-        ['rounded-inregular-1', { 'border-radius': '83% 34% 50% 61% / 62% 56% 65% 50%' }],
-        ['rounded-inregular-2', { 'border-radius': '35% 79% 50% 45% / 69% 92% 26% 35%' }],
-        ['lh-tighter', { 'line-height': '0.8' }],
-        ['text-last-justify', {'text-align-last': 'justify'}],
-        ['font-serif-naux', {'font-family': 'ui-serif, Georgia, Cambria, "Times New Roman", Times, Helvetica, serif'}],
-        ['font-chinese', {'font-family': 'SourceHanSerifHK'}]
-      ]
-    })
+    Unocss()
   ]
 });
---
+++ uno.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, presetAttributify, presetUno } from 'unocss';
+import presetIcons from '@unocss/preset-icons'
+import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig({
+  presets: [
+    presetAttributify(),
+    presetUno({
+      dark: 'class',
+    }),
+    presetIcons({
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block'
+      }
+    })
+  ],
+  transformers: [
+    transformerDirectives()
+  ],
+  rules: [
+    ['rounded-inregular-1', { 'border-radius': '83% 34% 50% 61% / 62% 56% 65% 50%' }],
+    ['rounded-inregular-2', { 'border-radius': '35% 79% 50% 45% / 69% 92% 26% 35%' }],
+    ['lh-tighter', { 'line-height': '0.8' }],
+    ['text-last-justify', {'text-align-last': 'justify'}],
+    ['font-serif-naux', {'font-family': 'ui-serif, Georgia, Cambria, "Times New Roman", Times, Helvetica, serif'}],
+    ['font-chinese', {'font-family': 'SourceHanSerifHK'}]
+  ]
+})

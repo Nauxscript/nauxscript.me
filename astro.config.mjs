cff2cf4c519f1b289ad3e0f39b43638212751cc7
nauxscript <nauxscript@gmail.com>
2023-09-05 16:58:05 +0800

    feat: use fontmin in plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import Unocss from '@unocss/astro'
+import { vitePluginAstroFontmin } from './vite-plugin-astro-fontmin'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     Unocss()
-  ]
+  ],
+  vite: {
+    plugins:[vitePluginAstroFontmin()]
+  }
 });
---
+++ vite-plugin-astro-fontmin/index.ts
@@ -0,0 +1,41 @@
+import { walk } from 'estree-walker'
+import { parse } from 'node-html-parser'
+import type {  PluginOption} from 'vite'
+import Fontmin from 'fontmin'
+
+let allText = ''
+
+export function vitePluginAstroFontmin(): PluginOption {
+  return {
+    name: 'vite-plugin-astro-fontmin',
+    enforce: 'pre', // post
+    apply: 'build', // apply 亦可以是一个函数
+    async transform(code, id) {
+      if (!id.endsWith('.astro'))
+        return
+      const ast = this.parse(code)
+      walk(ast as any, {
+        enter(node) {
+    
+          if (node.type === 'TemplateElement') {
+            allText += parse(node.value.raw).rawText          
+          }
+        },
+      }) 
+    },
+    async buildEnd() { 
+      if (!allText.trim()) {
+        console.log('[warning] minimize font done')
+        return
+      }
+      const fontmin = new Fontmin()
+      fontmin.src('fonts/SourceHanSerifTC-Bold.ttf')
+      fontmin.use(Fontmin.glyph({
+        text: allText,
+      }))
+      fontmin.dest('dist/fonts')
+      await fontmin.runAsync()
+      console.log('[success] minimize font done')
+    }
+  }
+}

1bb7a53fdbfb5afa059120d2f81fe5c2826a5326
nauxscript <nauxscript@gmail.com>
2023-11-03 11:41:08 +0800

    feat: mobile view

--- uno.config.ts
+++ uno.config.ts
@@ -18,6 +18,12 @@ export default defineConfig({
   transformers: [
     transformerDirectives()
   ],
+  shortcuts: {
+    'naux-logo': 'font-bold text-3.75rem bg-clip-text font-bold text-50px text-transparent from-gray-700 from-55% to-transparent',
+    'logo-effect': 'bg-gradient-to-b my-0 mb-[-14px]',
+    'logo-effect-md': 'bg-gradient-to-l mx-0 ml-[-14px]',
+    'logo-dark': 'from-stone-300 from-55% to-neutral-600',
+  },
   rules: [
     ['rounded-inregular-1', { 'border-radius': '83% 34% 50% 61% / 62% 56% 65% 50%' }],
     ['rounded-inregular-2', { 'border-radius': '35% 79% 50% 45% / 69% 92% 26% 35%' }],
@@ -25,6 +31,8 @@ export default defineConfig({
     ['text-last-justify', {'text-align-last': 'justify'}],
     ['font-serif-naux', {'font-family': 'ui-serif, Georgia, Cambria, "Times New Roman", Times, Helvetica, serif'}],
     ['font-chinese', {'font-family': 'SourceHanSerif'}],
-    ['mobile-text', {'writing-mode': 'vertical-rl'}]
+    ['mobile-text', {'writing-mode': 'vertical-rl'}],
+    // ['logo-gradient-to-l', {'background-image': 'linear-gradient(to left, #000, 55%, transparent)'}],
+    // ['logo-gradient-to-b', {'background-image': 'linear-gradient(to bottom, #000, 55%, transparent)'}]
   ]
 })

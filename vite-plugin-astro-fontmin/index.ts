310e25a0a2c1e65d6a1f6af412e4ac54ec81d161
nauxscript <nauxscript@gmail.com>
2023-12-12 17:17:14 +0800

    fix: post font

--- vite-plugin-astro-fontmin/index.ts
+++ vite-plugin-astro-fontmin/index.ts
@@ -11,13 +11,20 @@ export function vitePluginAstroFontmin(): PluginOption {
     enforce: 'pre', // post
     apply: 'build', // apply 亦可以是一个函数
     async transform(code, id) {
-      if (!id.endsWith('.astro'))
+      let fileType = ''
+      id.endsWith('.astro') && (fileType = 'astro')
+      id.endsWith('.md') && (fileType = 'md')
+      if (fileType === '')
         return
       const ast = this.parse(code)
       walk(ast as any, {
         enter(node) {
-          if (node.type === 'TemplateElement') {
+          if (fileType === 'astro' && node.type === 'TemplateElement') {
             allText += parse(node.value.raw).rawText          
+          }
+
+          if (fileType === 'md' && node.type === 'Literal') {
+            allText += parse(node.value?.toString() || '').rawText          
           }
         },
       }) 
@@ -26,6 +33,9 @@ export function vitePluginAstroFontmin(): PluginOption {
       if (!allText.trim()) {
         console.log('[warning] minimize font fail, have no valid text')
         return
+      }
+      if (import.meta.env.VITE_LOG_FONT) {
+        console.log(allText.trim())
       }
       const fontmin = new Fontmin()
       fontmin.src('public/fonts/SourceHanSerifTC-Bold.ttf')

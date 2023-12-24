e60a647649d6299c91e38322f39ba1312f517483
nauxscript <nauxscript@gmail.com>
2023-12-24 13:37:28 +0800

    fix: dateTransform bug

--- src/utils/dateTransform.ts
+++ src/utils/dateTransform.ts
@@ -1,7 +1,7 @@
 
 const monthArray = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '拾月', '拾壹月', '拾贰月']
 const yearArray = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
-const dayArray = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
+const dayArray = ['', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖', '拾']
 
 export function dateTransform(dateStr: string) {
   const date = new Date(dateStr)
@@ -11,6 +11,6 @@ export function dateTransform(dateStr: string) {
     year += yearArray[parseInt(yearStr.charAt(i))];
   }
   const month = monthArray[date.getMonth()];
-  const day = dayArray[Math.floor(date.getDate() / 10)] + (date.getDate() % 10 === 0 ? '十' : dayArray[date.getDate() % 10]);
+  const day = (date.getDate() > 19 ? dayArray[Math.floor(date.getDate() / 10)] : '') + (date.getDate() > 10 ? '拾' : '') + dayArray[date.getDate() % 10];
   return ` ${year}年${month}${day}日`
 }

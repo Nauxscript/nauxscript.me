3da3ea754cfec441795e549cb27b130aa236f5b5
nauxscript <nauxscript@gmail.com>
2024-01-23 10:55:30 +0800

    fix: code highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,30 @@ export default defineConfig({
     Unocss()
   ],
   vite: {
-    plugins:[vitePluginAstroFontmin()]
+    plugins:[
+      vitePluginAstroFontmin(),
+    ]
+  },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Alternatively, provide multiple themes
+      // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
+      experimentalThemes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
+      // Find common transformers: https://shikiji.netlify.app/packages/transformers
+      transformers: [],
+    },
   }
 });

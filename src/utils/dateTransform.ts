e705ffe9321e395fe1c83649f02466408fa02717
nauxscript <nauxscript@gmail.com>
2024-01-28 17:58:33 +0800

    feat: Article list grouped by year
    closed #20

--- src/utils/dateTransform.ts
+++ src/utils/dateTransform.ts
@@ -1,9 +1,15 @@
 
-const monthArray = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '拾月', '拾壹月', '拾贰月']
+const monthArray = ['壹', '貳', '叄', '肆', '伍', '陸', '柒', '捌', '玖', '拾', '拾壹', '拾贰']
 const yearArray = ['零', '壹', '貳', '叄', '肆', '伍', '陸', '柒', '捌', '玖']
 const dayArray = ['', '壹', '貳', '叄', '肆', '伍', '陸', '柒', '捌', '玖', '拾']
 
-export function dateTransform(dateStr: string) {
+export function dateTransform(dateStr: string): string 
+export function dateTransform(dateStr: string, obj: true): {
+  year: string
+  month: string
+  day: string
+} 
+export function dateTransform(dateStr: string, obj = false) {
   const date = new Date(dateStr)
   let year = '';
   let yearStr = date.getFullYear().toString();
@@ -12,5 +18,12 @@ export function dateTransform(dateStr: string) {
   }
   const month = monthArray[date.getMonth()];
   const day = (date.getDate() > 19 ? dayArray[Math.floor(date.getDate() / 10)] : '') + (date.getDate() > 10 ? '拾' : '') + dayArray[date.getDate() % 10];
+  if (obj) {
+    return {
+      year: `${year}年`,
+      month: `${month}月`,
+      day: `${day}日`
+    }
+  }
   return ` ${year}年${month}${day}日`
 }
